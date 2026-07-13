457b5c59a46bd1440b90e32a82b3929cb5bbe71f
AbiramiAS <abiramishanmugamvkl@gmail.com>
2026-07-13 16:53:50 +0530

    Add payment dialog box added

--- supplier-app/src/AddPayments.jsx
+++ supplier-app/src/AddPayments.jsx
@@ -1,4 +1,19 @@
+import {useState} from 'react';
+import supplierList from './SupplierList.json';
 function AddPayments() {
+          const [supplierName, setSupplierName] = useState("Select Supplier");
+          const [mainBalance, setMainBalance] = useState(0);
+          const handleVendorSelection = (event) => {
+          setSupplierName(event.target.value);
+          const selectedSupplier =
+                    supplierList.filter(
+                    (list) => list.supplierName == event.target.value
+                    );
+               setMainBalance(selectedSupplier[0].main_balance);     
+          }         
+          const handleSaveChanges = () => {
+
+          }
           return (
             <>
               <button
@@ -11,30 +26,70 @@ function AddPayments() {
               </button>
 
               <div
-                class="modal fade"
+                className="modal fade"
                 id="paymentModal"
-                tabindex="-1"
+                tabIndex="-1"
                 aria-labelledby="paymentModalLabel"
                 aria-hidden="true"
               >
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h6 class="modal-title" id="paymentModalLabel">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h6 className="modal-title" id="paymentModalLabel">
                         Add Payment
                       </h6>
                       <button
                         type="button"
-                        class="btn-close"
+                        className="btn-close"
                         data-bs-dismiss="modal"
                         aria-label="Close"
                       ></button>
                     </div>
-                                                            <div class="modal-body">
-                                                                      
+                    <div className="modal-body">
+                      <div>
+                        <label>Vendor</label>
+                      </div>
+                      <div className="dropdown">
+                        <button
+                          className="btn btn-secondary dropdown-toggle"
+                          type="button"
+                          id="supplierDropdown"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          {supplierName}
+                        </button>
+                        <ul
+                          className="dropdown-menu"
+                          aria-labelledby="supplierDropdown"
+                        >
+                          {supplierList &&
+                            supplierList.map((entry) => (
+                              <li key={entry.supplier_id}>
+                                <button
+                                  className="dropdown-item"
+                                  type="button"
+                                  value={entry.supplierName}
+                                  onClick={handleVendorSelection}
+                                >
+                                  {entry.supplierName}
+                                </button>
+                              </li>
+                            ))}
+                        </ul>
+                      </div>
+                      {supplierName && (
+                        <div className="alert alert-info" role="alert">
+                          Main Balance is : {mainBalance} 
+                        </div>
+                      )}
                     </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-primary">
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={handleSaveChanges}
+                      >
                         Save changes
                       </button>
                     </div>
